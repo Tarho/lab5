7e02e6c006e5c6eb281c6f86503c24d6eeb9f38b
Tarho <115691708+Tarho@users.noreply.github.com>
2023-12-27 22:42:07 +0700

    Update SidebarChat.js

--- createmessageapp/createmessageapp_fe/src/SidebarChat.js
+++ createmessageapp/createmessageapp_fe/src/SidebarChat.js
@@ -5,7 +5,7 @@ import "./SidebarChat.css";
 function SidebarChat() {
   return (
     <div className="sidebarChat">
-      <Avatar src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.15752-9/413062748_1461747724375134_734170550344977335_n.png?_nc_cat=105&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFvGYILxxhRmZqZKN7ijqapjySiuroPLJePJKK6ug8slwmWsKmdka9H8rdr2Xit-qt5cG7Ou8WNrVyDMxxO0atf&_nc_ohc=B4ppa0gBz2sAX_fDCRn&_nc_ht=scontent.fsgn5-2.fna&oh=03_AdSfO6WeKS8xkD_00H0gRGL_gbwCIzdo0gl_vnEtuuLIQg&oe=65B3A2BE" />
+      <Avatar src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/405308691_3492221227775223_5410689100886905515_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHtScb6Spi-zxkVH9xrWvAZEgUcK_Ahg3kSBRwr8CGDeeD-yqvuErgSlw2mC3vWfOa1hxWwn7fXQ3c_siRPR8PH&_nc_ohc=pQJivxO-sbsAX9KR7gX&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfBiaQ0nFWbKL-AVURrGXWDQlTgLsOGvExUUUjnYtgj8ow&oe=659186CC" />
       <div className="sidebarChat__info">
         <h2>Random person</h2>
         <p>This is the last message.</p>
